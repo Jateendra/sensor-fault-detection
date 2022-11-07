6c15bdd70055210837eda2374e1fccdf10087cc3
Jateendra <jateendra.pradhan@gmail.com>
2022-11-07 14:50:32 +0530

    bucket name naming

--- sensor/constant/s3_bucket.py
+++ sensor/constant/s3_bucket.py
@@ -1,2 +1,2 @@
-TRAINING_BUCKET_NAME = "scania-sensor-pipeline"
-PREDICTION_BUCKET_NAME = "sensor-datasource"
+TRAINING_BUCKET_NAME = "sensor-pipeline-jateen-001"
+PREDICTION_BUCKET_NAME = "sensor-datasource-jateen-001"
